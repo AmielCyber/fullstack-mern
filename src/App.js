2a350ea9280067bc917e868ba92e87c7477af4fe
Amiel Nava <amieln@icloud.com>
2022-10-20 12:12:02 -0700

    Added Navigation header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
