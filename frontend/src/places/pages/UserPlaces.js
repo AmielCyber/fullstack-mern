a7910f313b72de96f9b62f5da4586b8b2173da11
Amiel Nava <amieln@icloud.com>
2023-02-08 18:10:23 -0800

    Added our useHttpClient hook for fetching data

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,43 +1,53 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // My imports.
 import PlaceList from "../components/PlaceList";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner/LoadingSpinner";
 
-// Dummy places to be later replaced with google maps.
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Golden Gate Bridge",
-    description: "San Francisco Golden Gate",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-    address: "Golden Gate Bridge, San Francisco, CA",
-    location: {
-      lat: 37.8199286,
-      lng: -122.4782551,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Golden Gate Bridge",
-    description: "San Francisco Golden Gate",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-    address: "Golden Gate Bridge, San Francisco, CA",
-    location: {
-      lat: 37.8199286,
-      lng: -122.4782551,
-    },
-    creator: "u2",
-  },
-];
+async function fetchPlaces(sendRequest, setLoadedPlaces, userId) {
+  try {
+    const responseData = await sendRequest(
+      `http://localhost:5050/api/places/user/${userId}`
+    );
+    if (responseData) {
+      setLoadedPlaces(responseData.places);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
 /**
  * Displays places from a user.
  * @returns UserPlaces component
  */
 function UserPlaces() {
+  const [loadedPlaces, setLoadedPlaces] = useState(null);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    fetchPlaces(sendRequest, setLoadedPlaces, userId);
+  }, [userId, sendRequest]);
+
+  const placeDeletedHandler = (deletedPlaceId) => {
+    setLoadedPlaces((prevPlaces) =>
+      prevPlaces.filter((place) => place.id !== deletedPlaceId)
+    );
+  };
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && (
+        <PlaceList items={loadedPlaces} onDelete={placeDeletedHandler} />
+      )}
+    </>
+  );
 }
 export default UserPlaces;
