1c1a404e846a201a99ac7e0b583afc04a6c3c6b7
Amiel Nava <amieln@icloud.com>
2023-02-08 18:07:00 -0800

    Modified the login and logout functions to pass the userId state that we made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,15 @@ import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const login = useCallback(() => {
+  const [userId, setUserId] = useState(false);
+
+  const login = useCallback((uId) => {
     setIsLoggedIn(true);
+    setUserId(uId);
   });
   const logout = useCallback(() => {
     setIsLoggedIn(false);
+    setUserId(null);
   });
 
   let routes;
@@ -41,7 +45,12 @@ function App() {
   }
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      value={{
+        isLoggedIn: isLoggedIn,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
     >
       <BrowserRouter>
         <MainNavigation />
