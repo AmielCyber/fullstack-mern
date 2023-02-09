123b7ce33e319f29213dc67b0fecca60be79a31c
Amiel Nava <amieln@icloud.com>
2023-02-09 14:54:45 -0800

    Added middleware to handle uploaded images and handle error middleware to rollback an image being uploaded while an error occurred.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import path from "path"; // Path
+import fs from "fs"; // File System.
 // My imports.
 import placesRoutes from "./routes/places-routes.js";
 import userRoutes from "./routes/user-routes.js";
@@ -18,6 +20,7 @@ app.use(express.json());
 
 // Register middleware to have access to our backend calls from our frontend.
 // Request, Response, Next
+
 app.use((req, res, next) => {
   // Add headers to respond.
   // Allows which domain have access.
@@ -38,6 +41,7 @@ app.use((req, res, next) => {
 // Register route middleware.
 app.use("/api/places", placesRoutes); // => /api/places/...
 app.use("/api/users", userRoutes); // => /api/users/...
+app.use("/uploads/images", express.static(path.join("uploads", "images"))); // For retrieving images. express.static point to which path to point
 
 // Register middleware that does not get response such as unsupported routes.
 app.use(() => {
@@ -46,6 +50,13 @@ app.use(() => {
 
 // Register middleware that catches any errors thrown.
 app.use((error, req, res, next) => {
+  // Check if we had a file as part of the request then delete it.
+  if (req.file) {
+    // Roll back file.
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    }); // Points to the file and deletes it.
+  }
   if (res.headersSent) {
     // Forward the error if we have already sent an error.
     return next(error);
