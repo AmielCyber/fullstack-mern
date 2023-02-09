97c469bf292c428085e0279dd282d0a4a892f44f
Amiel Nava <amieln@icloud.com>
2023-02-08 18:04:05 -0800

    Added userId property

--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -2,6 +2,7 @@ import { createContext } from "react";
 // Authentication Context to check if a user is logged in.
 export const AuthContext = createContext({
   isLoggedIn: false,
+  userId: null,
   login: () => {},
   logout: () => {},
 });
