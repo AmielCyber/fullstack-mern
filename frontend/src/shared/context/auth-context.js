5f2671cf51d88bfb8349217d55159a227bf55d9a
Amiel Nava <amieln@icloud.com>
2023-02-09 22:15:23 -0800

    Added token to our authcontext

--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -2,6 +2,7 @@ import { createContext } from "react";
 // Authentication Context to check if a user is logged in.
 export const AuthContext = createContext({
   isLoggedIn: false,
+  token: null,
   userId: null,
   login: () => {},
   logout: () => {},
